8ba5912d89c01e5870a1d8b6368d02e7b55d48b7
Rami-J <rami2162@hotmail.com>
2021-10-31 22:23:28 -0400

    Remove commented code

--- server/app.js
+++ server/app.js
@@ -7,12 +7,6 @@ const port = process.env.PORT || 3001;
 app.use(express.static(path.join(__dirname, '..', 'build')));
 app.use(express.static('public'));
 
-/*if (process.env.NODE_ENV === 'production') {
-	app.get('*', (req, res) => {
-		res.sendFile(path.resolve(__dirname, 'build', 'index.html'));
-	});
-}*/
-
 app.use((req, res, next) => {
 	res.sendFile(path.join(__dirname, '..', 'build', 'index.html'));
 });
