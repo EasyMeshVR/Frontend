6fb6957bce49dea0f5ad557dfaef532df7bda695
Rami-J <rami2162@hotmail.com>
2021-12-20 18:47:08 -0500

    Basic upload page css and html

--- src/pages/Upload/UploadPage.js
+++ src/pages/Upload/UploadPage.js
@@ -1,9 +1,27 @@
 import React from 'react';
+import './UploadPage.css';
 
 const UploadPage = () => {
+	const handleFileDrop = (event) => {
+		event.preventDefault();
+		console.log(event);
+	};
+
+	const handleFileDrag = (event) => {
+		console.log(event);
+	};
+
 	return (
-		<div>
-			Upload	
+		<div className="UploadPage">
+			<h1>Upload STL model file</h1>
+			<div className="UploadDropZone" onDrop={(event) => handleFileDrop(event)} onDrag={handleFileDrag}>
+				<p className="DropZoneText">Drag an STL model file to drop zone or browse.</p>
+				<input 
+					className="DropZoneText" 
+					type="file"
+					accept="model/stl"
+				/>
+			</div>	
 		</div>
 	)
 };
