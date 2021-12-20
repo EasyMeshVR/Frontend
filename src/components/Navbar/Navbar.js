4e16b972e6f20cdc1f5a826295211b9d794adf33
Rami-J <rami2162@hotmail.com>
2021-12-20 14:42:24 -0500

    Add responsive navbar which dynamically shows a collapsible hamburger menu on 650px max-width

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,18 +1,47 @@
-import React from 'react';
+import React, { useState } from 'react';
 import NavLink from './NavLink/NavLink';
 import logoImg from '../../resources/images/logo.svg';
 import './Navbar.css';
 
 const Navbar = () => {
+	const [menuToggled, setMenuToggled] = useState(false);
+
+	const renderNavLinks = () => (
+		<>
+			<NavLink to="/download" classes="NavLinkRegular" text="Download" />
+		    <NavLink to="/upload" classes="NavLinkRegular" text="Upload" />
+		    <NavLink to="/about" classes="NavLinkRegular" text="About" />
+		</>
+	);
+
+	const renderHamburgerMenuLinks = () => (
+		(menuToggled ? 
+			<div className="HamburgerMenuLinksDiv">
+				{renderNavLinks()}
+			</div> :
+			<></>
+		)
+	);
+
 	return (
 		<nav className="Navbar">
-			<div className="NavLinkDiv">
+			<div className="BrandNameDiv">
 			    <NavLink to="/" classes='NavLinkLarge NavLinkBold' imgSrc={logoImg} text="EasyMeshVR" />
-			    <NavLink to="/download" classes="NavLinkRegular" text="Download" />
-			    <NavLink to="/upload" classes="NavLinkRegular" text="Upload" />
 			</div>
 			<div className="NavLinkDiv">
-			    <NavLink to="/about" classes="NavLinkRegular" text="About Us" />
+				{renderNavLinks()}
+			</div>
+			<div className="HamburgerMenuDiv">
+	            <a 
+					href="#" 
+					onClick={() => setMenuToggled(!menuToggled)} 
+					className="MenuToggleButton"
+				>
+	                <span className="MenuBar"></span>
+	                <span className="MenuBar"></span>
+	                <span className="MenuBar"></span>
+	            </a>
+				{renderHamburgerMenuLinks()}
 			</div>
 		</nav>
 	)
