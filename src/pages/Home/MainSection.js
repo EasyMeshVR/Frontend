e8d0933f38f53663f55cf6bc07430a94e7dd85a2
Rami <44623786+Rami-J@users.noreply.github.com>
2022-02-26 15:42:58 -0500

    Add video to homepage and fix responsiveness (#9)

--- src/pages/Home/MainSection.js
+++ src/pages/Home/MainSection.js
@@ -1,39 +1,45 @@
 import React from 'react';
 import mainSectionStyles from './MainSection.module.css';
-import deviceMockupImage from '../../resources/images/iphone-mockup.png';
 
 const MainSection = () => {
 	return (
 		<section className={mainSectionStyles.Hero}>
-			<div className={mainSectionStyles.Container}>
-				<div className={mainSectionStyles.HeroInner}>
-					<div className={mainSectionStyles.HeroCopy}>
-						<h1 className={`${mainSectionStyles.HeroTitle} ${mainSectionStyles.mt0}`}>
-							EasyMeshVR
-						</h1>
-						<p className={mainSectionStyles.HeroParagraph}>
-							Design and edit STL meshes within virtual reality, 
-							collaborate with others, and download/upload your STL models using our
-							website or within our VR application.
-						</p>
-						<div className={mainSectionStyles.HeroCta}>
-							<a 
-								className={`${mainSectionStyles.Button} ${mainSectionStyles.ButtonShadow}`} 
-								href="/"
-							>
-								Learn more
-							</a>
-							<a 
-								className={`${mainSectionStyles.Button} ${mainSectionStyles.ButtonPrimary} ${mainSectionStyles.ButtonShadow}`} 
-								href="/"
-							>
-								Download App
-							</a>
-						</div>
+			<div className={mainSectionStyles.HeroInner}>
+				<div className={mainSectionStyles.HeroCopy}>
+					<h1 className={`${mainSectionStyles.HeroTitle} ${mainSectionStyles.mt0}`}>
+						EasyMeshVR
+					</h1>
+					<p className={mainSectionStyles.HeroParagraph}>
+						Design and edit STL meshes within virtual reality, 
+						collaborate with others, and download/upload your STL models using our
+						website or within our VR application.
+					</p>
+					<div className={mainSectionStyles.HeroCta}>
+						<a 
+							className={`${mainSectionStyles.Button} ${mainSectionStyles.ButtonShadow}`} 
+							href="/"
+						>
+							Learn more
+						</a>
+						<a 
+							className={`${mainSectionStyles.Button} ${mainSectionStyles.ButtonPrimary} ${mainSectionStyles.ButtonShadow}`} 
+							href="/"
+						>
+							Download App
+						</a>
 					</div>
-					{/* TODO: mockups/screenshots of the VR application  */}
-					<img className={mainSectionStyles.DeviceMockup} src={deviceMockupImage} alt="App preview" />
 				</div>
+				<iframe 
+					className={mainSectionStyles.DeviceMockup}
+					width="560" 
+					height="315" 
+					src="https://www.youtube.com/embed/i0Ctz1MZ3yk" 
+					title="YouTube video player" 
+					frameborder="0" 
+					allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" 
+					allowfullscreen
+				>
+				</iframe>
 			</div>
 		</section>
 	)
