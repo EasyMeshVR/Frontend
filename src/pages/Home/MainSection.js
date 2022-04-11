acb9e9b1ae5d550b9eaa34eaf1c8056b8c306393
Rami Jurdi <rami2162@hotmail.com>
2022-04-11 16:04:01 -0400

    Edit download app release link

--- src/pages/Home/MainSection.js
+++ src/pages/Home/MainSection.js
@@ -23,7 +23,7 @@ const MainSection = () => {
 						</a> */}
 						<a 
 							className={`${mainSectionStyles.Button} ${mainSectionStyles.ButtonPrimary} ${mainSectionStyles.ButtonShadow}`} 
-							href="https://github.com/EasyMeshVR/EasyMeshVR-Prototype/releases/tag/v1.0.0"
+							href="https://github.com/EasyMeshVR/EasyMeshVR-Prototype/releases"
 						>
 							Download App
 						</a>
